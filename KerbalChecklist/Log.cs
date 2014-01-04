0ea823d2c159369fcf2f1357c306932b93713a3e
Ingo Bürk <admin@airblader.de>
2014-01-04 16:44:19 -0500

    added wrapper class for logging

---
+++ KerbalChecklist/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tac;
+
+namespace KerbalChecklist {
+
+    static class Log {
+
+        private const string KERBAL_CONTEXT = "KerbalChecklist";
+
+        public static void Debug( string message ) {
+            Logging.Log( KERBAL_CONTEXT, message );
+        }
+
+        public static void Error( string message, Exception e = null ) {
+            string compositeMessage = message;
+            if( e != null ) {
+                compositeMessage += " [" + e.Message + "]: " + e.StackTrace;
+            }
+
+            Logging.LogError( KERBAL_CONTEXT, compositeMessage );
+        }
+
+    }
+
+}
