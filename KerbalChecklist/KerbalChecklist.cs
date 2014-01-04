c142fc377f917e132f58e15a4c358b414982925c
Ingo Bürk <admin@airblader.de>
2014-01-04 13:45:36 -0500

    added toolbar button

--- KerbalChecklist/KerbalChecklist.cs
+++ KerbalChecklist/KerbalChecklist.cs
@@ -12,15 +12,26 @@ namespace KerbalChecklist {
         private Checklists checklists; // TODO rename this
         private ChecklistWindow checklistWindow;
 
+        private ButtonWrapper toolbarButton;
+
         void Awake() {
             checklists = Checklists.Load( CONFIG_FILENAME );
             checklistWindow = new ChecklistWindow( checklists );
 
-            // TODO add Tooltip button
+            SetupToolbar();
         }
 
         void Start() {
             checklistWindow.SetVisible( true );
+        }
+
+        private void SetupToolbar() {
+            toolbarButton = new ButtonWrapper( new Rect( Screen.width * 0.7f, 0, 32, 32 ),
+                "KerbalChecklist/kerbalchecklist", "KC", "Kerbal Checklist", checklistWindow.ToggleVisible );
+        }
+
+        internal void OnDestroy() {
+            toolbarButton.Destroy();
         }
 
     }
